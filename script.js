8e43ed22ee225efbe4b7e8e3da9b929850a0c631
AmritpalBedi <125513113+AmritpalBedi@users.noreply.github.com>
2023-10-10 20:16:54 -0500

    Add pause and next without functions

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let workTitle = document.getElementById("work");
 let breakTitle = document.getElementById("break");
 
-let workTime = 25;
+let workTime = 2;
 let breakTime = 5;
 
 let seconds = "00";
@@ -20,10 +20,12 @@ window.onload = () => {
 function start() {
     // change button
     document.getElementById("start").style.display= "none";
-    document.getElementById("reset").style.display= "block";
+    document.querySelector(".work-icons").style.display= "flex";
+    document.getElementById("next").style.display= "none";
+    
 
     // change time
-    seconds = 59;
+    seconds = 15;
 
     let workMinutes = workTime - 1;
     let breakMinutes =  breakTime - 1;
@@ -32,31 +34,25 @@ function start() {
 
     // countdown
     let timerFunction = () => {
+        // Display current time
         document.getElementById("minutes").innerHTML = workMinutes;
-        document.getElementById("seconds").innerHTML = seconds;
 
+        if (seconds < 10) {
+            document.getElementById("seconds").innerHTML = "0" + seconds;
+        } else {
+            document.getElementById("seconds").innerHTML = seconds;
+        }
+        
         seconds = seconds -  1;
 
         if (seconds == -1) {
             workMinutes = workMinutes - 1;
-            if (workMinutes == -1) {
-                if (breakCount % 2 == 0) {
-                    workMinutes = breakMinutes;
-                    breakCount++;
-
-                    workTitle.classList.remove("active");
-                    breakTitle.classList.add("active");
-                } else {
-
-                    workMinutes = workTime;
-                    breakCount++;
-
-                    breakTitle.classList.remove("active");
-                    workTitle.classList.add("active");
-                    
-                }
+            document.getElementById("next").style.display= "block";
+            if (workMinutes >= 0) {
+                seconds = 59;
+            } else {
+                // Add overtime 
             }
-            seconds = 59;
         }
     }
 
